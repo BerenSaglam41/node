35fe899b6c72e7e31197c2473fbef2a61ab62a7f
BerenSaglam41 <mustafaberensaglam9@gmail.com>
2025-01-08 00:28:23 +0300

    Api Link Details and added top-5-cheap shortcut for filters
    Read me added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,20 +1,60 @@
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = async (req,res,next) =>{
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,difficulty,'
+    next();
+};
+
 exports.getAllTours =async (req,res)=>{
     try{
-        const tours = await Tour.find();
+        // 1 ) Filtering
+        const queryObj = {...req.query};
+        const excludedFields = ['page','sort','limit','fields'];
+        excludedFields.forEach(el => delete queryObj[el]);
+        // 2) Advance Filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g,match => `$${match}`);
+        let query = Tour.find((JSON.parse(queryStr)));
+        // 3 ) Sorting 
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        }
+        else{
+            query = query.sort('-createdAt');
+        }
+        // 4) Field Limiting
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);  // select işlemi burada yapılır
+        } else {
+            query = query.select('name duration price ratingsAverage createdAt');  // Varsayılan alanları seç
+        }
+        // 5)Pagination
+        const page = req.query.page *1 || 1;
+        const limit = req.query.limit * 1|| 100;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
+        if(req.query.page){
+            const numTours = await Tour.countDocuments();
+            if(skip >= numTours) throw new Error('This page does not exists 🤷');
+        }
+        // EXECUTE Query
+        const tours =await query;
     res.status(200).json({
-       status: 'Succes',
-       results : tours.length,
-       data:{
-           tours
-       }
+    status: 'Succes', 
+    results : tours.length,
+    data:{
+        tours
+    }
     });
     }
     catch(err){
         res.status(400).json({
             status:'Failed',
-            message: err
+            message: err.message
         })
     }
 };
