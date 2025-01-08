8db79f1ba2f3147f908aa70d34192f90de8269ec
BerenSaglam41 <mustafaberensaglam9@gmail.com>
2025-01-08 15:02:02 +0300

    Document MiddleWare

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 // turlar için klasik belirlenen şemayı hazırla
 const tourSchema = new mongoose.Schema({
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema({
         unique : true,
         trim : true
     },
+    slug : String,
     duration : {
         type : Number,
         required : [true,'A tour must have a duration']
@@ -51,8 +53,51 @@ const tourSchema = new mongoose.Schema({
         type : Date,
         default : Date.now()
     },
-    startDates : [Date]
+    startDates : [Date],
+    secretTour : {
+        type : Boolean,
+        default : false
+    }
+},{
+    // virtual için aratırken görüntü verir
+    toJSON : { virtuals : true},
+    toObject : { virtual : true }
+}
+);
+
+// virtual aratırken görünür kaydedlmez
+tourSchema.virtual('durationWeeks').get(function (){
+    return this.duration/7;
 });
+
+// Document Middleware runs before the .save() and .create() methods
+tourSchema.pre('save',function (next){
+    this.slug = slugify(this.name, { lower:true});
+    next();
+});
+
+// tourSchema.pre('save',function (next){
+//     console.log('Will save document...');
+//     next();
+// });
+
+// tourSchema.post('save',function(doc,next){
+//     console.log(doc);
+//     next();
+// })
+
+        // QUERY MİDDLEWARE if request is find secret tours are hidden 
+tourSchema.pre(/^find/,function(next){
+    this.find({ secretTour : {$ne : 'true'}});
+    this.start = Date.now();
+    next();
+});
+
+tourSchema.post(/^find/,function(docs,next){
+    console.log(`Query took ${Date.now() - this.start} miliseconds.`);
+    next();
+});
+
 
 // turlar için hazırlanan şemayı Tour adında bi model oluşturup ona kaydet
 const Tour = mongoose.model('Tour', tourSchema);
