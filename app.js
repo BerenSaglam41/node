685ad85a57cb48f6380b47d024772e082ce4a701
BerenSaglam41 <mustafaberensaglam9@gmail.com>
2025-01-13 22:40:25 +0300

    Secuity Starts -Reqest limit => 100

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
+const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const appError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController')
 
-// 1)Middlewares
+// 1)GLOBAL Middlewares
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
+// for limit server requests for attacks.
+const limiter = rateLimit({
+    max : 100,
+    windowMs : 60 * 60 * 1000,
+    message : 'Too many requests from this IP , please try again in a hour!'
+});
+app.use('/api',limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
