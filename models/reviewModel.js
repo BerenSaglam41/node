d3853538006f234fc4447e117f814b25d4f01765
BerenSaglam41 <mustafaberensaglam9@gmail.com>
2025-01-19 14:44:59 +0300

    stats updates

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
     {
@@ -49,6 +50,36 @@ reviewSchema.pre(/^find/,async function(next){
 });
 
 
+reviewSchema.statics.calcAverageRatings = async function(tourId){
+    const stats = await this.aggregate([
+        {
+            $match : { tour : tourId}
+        },
+        {
+            $group : {
+                _id : '$tour',
+                nRating : { $sum : 1},
+                avgRating : {$avg : '$rating'}
+            }
+        }
+    ]);
+    await Tour.findByIdAndUpdate(tourId,{
+        ratingsQuantity : stats[0].nRating,
+        ratingsAverage : stats[0].avgRating 
+    });
+};
+
+reviewSchema.post('save',function(){
+    // this points to current review,
+    this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/,async function(next){
+    const r = await this.findOne();
+    console.log(r);
+});
+
 const Review = mongoose.model('Review',reviewSchema);
+
 
 module.exports = Review;
